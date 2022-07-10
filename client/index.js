8e80421d3df42679e21c79bbced31411ca58b3e2
Adarsh Chourasia <adarsh.chourasia@scaler.com>
2022-07-10 11:26:48 +0530

    Json Response now extracted from API call

--- client/index.js
+++ client/index.js
@@ -1,89 +1,95 @@
-const select_fees = document.getElementById("fees");
-let fee_type = null;
+fetch("http://localhost:3000/getUsers")
+  .then((response) => response.json())
+  .then((data) => calculatePayment(data));
 
-select_fees.options.add(new Option("Select fees type", "Select fees type"));
-Object.keys(data).forEach(function (key) {
-  let option;
-  option = document.createElement("option");
-  option.text = key;
-  option.value = key;
-  select_fees.add(option);
-});
+function calculatePayment(data) {
+  const select_fees = document.getElementById("fees");
+  let fee_type = null;
 
-select_fees.addEventListener("change", function handleChange(event) {
-  fee_type = event.target.value;
-  document.getElementById("nationality").removeAttribute("hidden");
-  document.getElementById("nationality_label").removeAttribute("hidden");
-
-  dropdown = document.getElementById("nationality");
-  document
-    .getElementById("nationality")
-    .options.add(new Option("Select nationality", "Select nationality"));
-
-  Object.keys(data[fee_type]).forEach(function (key) {
+  select_fees.options.add(new Option("Select fees type", "Select fees type"));
+  Object.keys(data).forEach(function (key) {
     let option;
     option = document.createElement("option");
     option.text = key;
     option.value = key;
-    dropdown.add(option);
+    select_fees.add(option);
   });
-  document.getElementById("fees").setAttribute("disabled", "");
-});
 
-const select_natioanlity = document.getElementById("nationality");
-let nationality_type = null;
+  select_fees.addEventListener("change", function handleChange(event) {
+    fee_type = event.target.value;
+    document.getElementById("nationality").removeAttribute("hidden");
+    document.getElementById("nationality_label").removeAttribute("hidden");
 
-select_natioanlity.addEventListener("change", function handleChange(event) {
-  nationality_type = event.target.value;
-  document.getElementById("course").removeAttribute("hidden");
-  document.getElementById("course_label").removeAttribute("hidden");
+    dropdown = document.getElementById("nationality");
+    document
+      .getElementById("nationality")
+      .options.add(new Option("Select nationality", "Select nationality"));
 
-  dropdown = document.getElementById("course");
-  document
-    .getElementById("course")
-    .options.add(new Option("Select course", "Select course"));
-
-  Object.keys(data[fee_type][nationality_type]).forEach(function (key) {
-    let option;
-    option = document.createElement("option");
-    option.text = key;
-    option.value = key;
-    dropdown.add(option);
+    Object.keys(data[fee_type]).forEach(function (key) {
+      let option;
+      option = document.createElement("option");
+      option.text = key;
+      option.value = key;
+      dropdown.add(option);
+    });
+    document.getElementById("fees").setAttribute("disabled", "");
   });
-  document.getElementById("nationality").setAttribute("disabled", "");
-});
 
-const select_course = document.getElementById("course");
-let course_type = null;
+  const select_natioanlity = document.getElementById("nationality");
+  let nationality_type = null;
 
-select_course.addEventListener("change", function handleChange(event) {
-  course_type = event.target.value;
-  document.getElementById("level").removeAttribute("hidden");
-  document.getElementById("level_label").removeAttribute("hidden");
+  select_natioanlity.addEventListener("change", function handleChange(event) {
+    nationality_type = event.target.value;
+    document.getElementById("course").removeAttribute("hidden");
+    document.getElementById("course_label").removeAttribute("hidden");
 
-  dropdown = document.getElementById("level");
-  document
-    .getElementById("level")
-    .options.add(new Option("Select level", "Select level"));
+    dropdown = document.getElementById("course");
+    document
+      .getElementById("course")
+      .options.add(new Option("Select course", "Select course"));
 
-  Object.keys(data[fee_type][nationality_type][course_type]).forEach(function (
-    key
-  ) {
-    let option;
-    option = document.createElement("option");
-    option.text = key;
-    option.value = key;
-    dropdown.add(option);
+    Object.keys(data[fee_type][nationality_type]).forEach(function (key) {
+      let option;
+      option = document.createElement("option");
+      option.text = key;
+      option.value = key;
+      dropdown.add(option);
+    });
+    document.getElementById("nationality").setAttribute("disabled", "");
   });
-  document.getElementById("course").setAttribute("disabled", "");
-});
 
-const select_level = document.getElementById("level");
-let level_type = null;
-select_level.addEventListener("change", function handleChange(event) {
-  let level_type = event.target.value;
-  var ans = document.getElementById("answer");
-  ans.innerHTML = JSON.stringify(
-    data[fee_type][nationality_type][course_type][level_type]
-  );
-});
+  const select_course = document.getElementById("course");
+  let course_type = null;
+
+  select_course.addEventListener("change", function handleChange(event) {
+    course_type = event.target.value;
+    document.getElementById("level").removeAttribute("hidden");
+    document.getElementById("level_label").removeAttribute("hidden");
+
+    dropdown = document.getElementById("level");
+    document
+      .getElementById("level")
+      .options.add(new Option("Select level", "Select level"));
+
+    Object.keys(data[fee_type][nationality_type][course_type]).forEach(
+      function (key) {
+        let option;
+        option = document.createElement("option");
+        option.text = key;
+        option.value = key;
+        dropdown.add(option);
+      }
+    );
+    document.getElementById("course").setAttribute("disabled", "");
+  });
+
+  const select_level = document.getElementById("level");
+  let level_type = null;
+  select_level.addEventListener("change", function handleChange(event) {
+    let level_type = event.target.value;
+    var ans = document.getElementById("answer");
+    ans.innerHTML = JSON.stringify(
+      data[fee_type][nationality_type][course_type][level_type]
+    );
+  });
+}
---
+++ server/server3.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var fs = require("fs");
+const cors = require("cors");
+app.use(cors());
+
+app.get("/getUsers", function (req, res) {
+  fs.readFile(__dirname + "/" + "test_data.json", "utf8", function (err, data) {
+    res.end(data);
+  });
+});
+
+var server = app.listen(3000, () => {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("API running", host, port);
+});
